14a49b331e00272472f6665bc08b6f35ec2f5810
hashfifahle000 <91356942+hashfifahle000@users.noreply.github.com>
2022-03-07 10:18:57 +0700

    Update image upscaling.py

--- Image Upscaling/image upscaling.py
+++ Image Upscaling/image upscaling.py
@@ -10,12 +10,16 @@ from cv2 import dnn_superres
 
 sr = dnn_superres.DnnSuperResImpl_create()
 
+#Source image path
 img = cv.imread('E:\\Documents\\COLLEGE\\PCV\\sample2.png')
 
+#Upscaling Model path
 path = "E:\\Documents\\COLLEGE\\PCV\\LapSRN_x4.pb"
 sr.readModel(path)
 sr.setModel("lapsrn", 4)
 
+#Upscale operation
 result = sr.upsample(img)
 
+#Save result as new image
 cv.imwrite("E:\\Documents\\COLLEGE\\PCV\sample2_upscaled_lapsrnx4.png", result)
