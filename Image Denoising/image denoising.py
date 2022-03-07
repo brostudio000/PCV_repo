d3040f40ff9452110ecc88d3cb4463f290b28216
hashfifahle000 <91356942+hashfifahle000@users.noreply.github.com>
2022-03-07 10:00:39 +0700

    Add files via upload

---
+++ Image Denoising/image denoising.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  2 06:31:57 2022
+
+@author: Bro
+"""
+
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+img = cv.imread('C:\\Users\\hp\\Desktop\\part7.png')
+
+dst = cv.fastNlMeansDenoisingColored(img,None,6,10,7,21)
+
+cv.imwrite("E:\\Documents\\COLLEGE\\PCV\\part7_denoised.png", dst)
+cv.imshow('frame1', img)
+cv.imshow('frame2', dst)
+ch = cv.waitKey(0) & 0xFF
+cv.destroyAllWindows()
